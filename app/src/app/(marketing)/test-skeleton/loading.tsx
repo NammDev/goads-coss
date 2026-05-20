a79fad491370c731b63e0605847cc93eedeb40df
Nguyen Nam Khanh <nammdev@Nguyens-MacBook-Pro.local>
2026-05-20 18:21:05 +0700

    chore: remove /test-skeleton demo route

--- app/src/app/(marketing)/test-skeleton/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { ForeplayProductPageSkeleton } from "@/components/foreplay/foreplay-product-page-skeleton"
-
-export default function Loading() {
-  return <ForeplayProductPageSkeleton />
-}
